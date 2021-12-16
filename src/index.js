49019ca9fc702984a5fbb05d07c9821cfc2dd678
Sokol2020 <mr.sokolsergey@gmail.com>
2021-12-16 22:04:47 +0200

    add calc.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import timer from './modules/timer'
 import smoothScroll from './modules/smoothScroll'
 import modal from './modules/modal'
+import calc from './modules/calc'
 
 
 timer(`23 december 2021`)
 smoothScroll()
 modal()
+checkForms()
+
+if (window.location.toString().indexOf("balkony.html") > 0) {
+  calc();
+}
+
